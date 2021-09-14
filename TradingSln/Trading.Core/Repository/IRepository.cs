0e595d3fd000a704090adf491e565413973e20b7
hari krishna <harikrishna2005@gmail.com>
2021-09-14 16:02:56 +0530

    EntityFrameworkTradingCore

---
+++ TradingSln/Trading.Core/Repository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq.Expressions;
+
+namespace Trading.Core.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        //Get 
+        TEntity Get(int Id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+        //Add
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        //Remove
+
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
